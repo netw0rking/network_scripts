90a202de56811ff4fa3e008d9ca873dab0693e31
wquizhpi <wquizhpi@packet.com>
2021-04-07 06:09:16 -0400

    uploading practice directory

---
+++ practice/show_commands.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+
+import paramiko
+import sys
+
+host = sys.argv[1]
+cmd = input('Type the command to send: ')
+
+def initConn(x):
+    ssh_client = paramiko.SSHClient()
+    ssh_client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+    ssh_client.connect(x,username='wquizhpi',password='Juniper',look_for_keys=False)
+    return ssh_client
+
+def sendcmd(sshHost,command):
+    stdin,stdout,stderr=sshHost.exec_command(command)
+    return stdout
+
+ssh_conn = initConn(host)
+
+output = sendcmd(ssh_conn,cmd)
+
+for items in output:
+    items = items.strip("\r\n")
+    print(items)
+
+ssh_conn.close()
+del ssh_conn
