1c7a09f6b71191c2956b2772dfa527e0590a0451
wquizhpi <wquizhpi@packet.com>
2021-04-08 09:42:34 -0400

    Moving some files between directories

--- paramiko/show_commands.py
+++ paramiko/show_commands.py
@@ -6,17 +6,23 @@ import sys
 host = sys.argv[1]
 cmd = input('Type the command to send: ')
 
-ssh_client = paramiko.SSHClient()
-ssh_client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-ssh_client.connect(host,username='wquizhpi',password='cisco',look_for_keys=False)
+def initConn(x):
+    ssh_client = paramiko.SSHClient()
+    ssh_client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+    ssh_client.connect(x,username='wquizhpi',password='cisco',look_for_keys=False)
+    return ssh_client
 
-stdin,stdout,stderr=ssh_client.exec_command(cmd)
+def sendcmd(sshHost,command):
+    stdin,stdout,stderr=sshHost.exec_command(command)
+    return stdout
 
-output = stdout.readlines()
+ssh_conn = initConn(host)
+
+output = sendcmd(ssh_conn,cmd)
+
 for items in output:
     items = items.strip("\r\n")
     print(items)
 
-ssh_client.close()
-
-del ssh_client, stdin, stdout, stderr
+ssh_conn.close()
+del ssh_conn
