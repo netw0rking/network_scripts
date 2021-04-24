a79f831ccef0b16619662c51f331af12ece9639c
wquizhpi <wquizhpi@packet.com>
2021-04-24 10:43:13 -0400

    updates

--- nornir/python_test.py
+++ nornir/python_test.py
@@ -1,9 +1,13 @@
 from nornir import InitNornir
 from nornir_utils.plugins.functions import print_result
 from nornir_napalm.plugins.tasks import napalm_get
+from pprint import pprint
 
-nr = InitNornir(config_file='config.yml', dry_run= True)
+nr = InitNornir(config_file='config.yml', dry_run=True)
 
-results = nr.run(task=napalm_get, getters=['facts','interfaces'])
+sw_output = nr.run(task=napalm_get, getters=['facts', 'get_bgp_neighbors'])
 
-print_result(results)
+for switch in sw_output:
+    bgp_dict= sw_output[switch][0].result
+    pprint(bgp_dict['get_bgp_neighbors']['global']['peers'])
+    #import pdb; pdb.set_trace()
