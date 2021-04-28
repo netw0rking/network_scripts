f7dcb9f65ad144e73f7667cb1e0fea4e0f520d30
wquizhpi <wquizhpi@packet.com>
2021-04-28 05:47:50 -0400

    adding some scripts

--- nornir/host_yml.py
+++ nornir/host_yml.py
@@ -6,17 +6,25 @@ if len(sys.argv) <= 1:
     print('Use: host_yml.py {site code}')
 
 def host_dict(site_hosts):
-    host_dict = {'cisco':{},'juniper':{}}
+    host_dict = {}
     for hostname in site_hosts:
         host = nb.dcim.devices.get(name=hostname)
         ip = str(host.primary_ip)
         host_plat = host['platform']['name']
         if host_plat == 'ios':
-            host_dict['cisco']['name'] = hostname
-            host_dict['cisco']['loopback'] = ip[:-3]
+            host_dict[hostname] ={}
+            host_dict[hostname]['name'] = hostname
+            host_dict[hostname]['loopback'] = ip[:-3]
+            host_dict[hostname]['platform'] = 'ios_group'
+            host_dict[hostname]['password'] = 'cisco'
+            host_dict[hostname]['cli'] = 'ios'
         elif host_plat == 'junos':
-            host_dict['juniper']['name'] = hostname
-            host_dict['juniper']['loopback'] = ip[:-3]
+            host_dict[hostname] ={}
+            host_dict[hostname]['name'] = hostname
+            host_dict[hostname]['loopback'] = ip[:-3]
+            host_dict[hostname]['platform'] = 'junos_group'
+            host_dict[hostname]['password'] = 'Juniper'
+            host_dict[hostname]['cli'] = 'junos'
     return(host_dict)
 
 nb = pynetbox.api(url='http://100.64.1.226:8000',
@@ -25,9 +33,9 @@ nb = pynetbox.api(url='http://100.64.1.226:8000',
 site_hosts = nb.dcim.devices.filter(site=sys.argv[1])
 
 hosts = host_dict(site_hosts)
-#import pdb; pdb.set_trace()
 
 file_loader = FileSystemLoader('templates')
 env = Environment(loader=file_loader)
 template = env.get_template('nornir_host_file')
 output = template.render(host_list=hosts)
+print(output)
--- nornir/python_test.py
+++ nornir/python_test.py
@@ -3,11 +3,11 @@ from nornir_utils.plugins.functions import print_result
 from nornir_napalm.plugins.tasks import napalm_get
 from pprint import pprint
 
-nr = InitNornir(config_file='config.yml', dry_run=True)
+nr = InitNornir(config_file='./inventory/config.yml', dry_run=True)
 
-sw_output = nr.run(task=napalm_get, getters=['facts', 'get_bgp_neighbors'])
+sw_output = nr.run(task=napalm_get, getters=['facts', 'get_interfaces_ip'])
 
+output= {}
 for switch in sw_output:
     bgp_dict= sw_output[switch][0].result
-    pprint(bgp_dict['get_bgp_neighbors']['global']['peers'])
-    #import pdb; pdb.set_trace()
+    pprint(bgp_dict)
