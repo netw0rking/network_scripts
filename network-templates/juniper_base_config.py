8722949f4212f840d85d0189dc11213527619a08
netw0rking <77863075+netw0rking@users.noreply.github.com>
2021-12-05 00:05:26 -0500

    adding juniper templat

---
+++ network-templates/juniper_base_config.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+from jinja2 import Environment, FileSystemLoader
+import pynetbox
+import sys
+import os
+
+
+if len(sys.argv) < 2:
+    print("Use: ./juniper_base_config.py hostname")
+    quit()
+
+def get_interfaces_ip(hostname):
+    host_intfs = nb.dcim.interfaces.filter(device=hostname)
+    intf_addrs = {}
+    for intf in host_intfs:
+        if intf.connected_endpoint:
+            ip = nb.ipam.ip_addresses.get(device=hostname,interface=intf.name,family=4)
+            intf_addrs[intf]=ip.address
+        elif 'lo0' in intf.name:
+            ip = nb.ipam.ip_addresses.get(device=hostname,interface=intf.name,family=4)
+            intf_addrs[intf]=ip.address
+    return intf_addrs
+ 
+
+nb_url = os.environ.get("nb_url")
+nb_token = os.environ.get("nb_token")
+nb = pynetbox.api(url=nb_url, token=nb_token)
+
+hostname = sys.argv[1]
+int_ip = get_interfaces_ip(hostname)
+file_loader = FileSystemLoader("templates")
+env = Environment(loader=file_loader)
+template = env.get_template("junos_template")
+output = template.render(intf=int_ip,hostname=hostname)
+print(output)
